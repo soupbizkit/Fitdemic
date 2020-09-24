3b5f16cddf5f90e9853a8adcde12acdd49456a17
GrowLLateral <gutierrezprieto.ma@gmail.com>
2020-09-24 16:39:02 -0500

    agregue el activity padecimientos y modifique activity de preguntas

---
+++ app/src/main/java/com/example/fitdemic/PadecimientosActivity.java
@@ -0,0 +1,15 @@
+package com.example.fitdemic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PadecimientosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_padecimientos);
+    }
+
+}
--- app/src/main/java/com/example/fitdemic/PreguntasActivity.java
+++ app/src/main/java/com/example/fitdemic/PreguntasActivity.java
@@ -2,14 +2,33 @@ package com.example.fitdemic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class PreguntasActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preguntas);
+    }
+
+    public void onCreateUser(View view){
+        //intent
+        Intent intent = new Intent(this,PadecimientosActivity.class);
+        //Aqui debemos de crear el usuario y mandarlo a la base de datos
+        //obteniendo La edad del usuario
+        EditText etEdad = (EditText)findViewById(R.id.etEdad);
+        String edadUsuario = etEdad.getText().toString();
+        //Obteniendo El Peso del Usuario
+        EditText etPeso = (EditText)findViewById(R.id.etPeso);
+        String pesoUsuario = etPeso.getText().toString();
+        //Obteniendo Si es Hombre o Mujer (Sexo)
+        RadioButton rHombre
+        //aqui enviamos el intent
+        startActivity(intent);
     }
 
     //función del botón, recaudar datos  del activity, conectar a base, guardar datos y cerrar conección a base
