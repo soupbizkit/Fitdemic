2188f39e4e9d6f79ef3f6442ff921d3321321991
GrowLLateral <gutierrezprieto.ma@gmail.com>
2020-09-24 17:55:35 -0500

    get intent

--- app/src/main/java/com/example/fitdemic/PadecimientosActivity.java
+++ app/src/main/java/com/example/fitdemic/PadecimientosActivity.java
@@ -2,6 +2,7 @@ package com.example.fitdemic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class PadecimientosActivity extends AppCompatActivity {
@@ -10,6 +11,7 @@ public class PadecimientosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_padecimientos);
+        Intent intent = getIntent();
     }
 
 }
