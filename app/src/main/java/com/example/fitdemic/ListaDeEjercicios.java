ee34984d9f27bc1b2201fbe0a9ee8e0862437ec5
Walter <walter.santacruz@correounivalle.edu.co>
2020-10-13 16:27:41 -0500

    agregue toolbar a la activity de lista de ejercicios

--- app/src/main/java/com/example/fitdemic/ListaDeEjercicios.java
+++ app/src/main/java/com/example/fitdemic/ListaDeEjercicios.java
@@ -1,6 +1,7 @@
 package com.example.fitdemic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,7 +12,13 @@ public class ListaDeEjercicios extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_de_ejercicios);
+
         Intent intent = getIntent();
         String tipoRutina =  intent.getStringExtra("tipoRutina");
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 }
